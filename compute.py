52a2fa7cc22b7953810dc997bb91d4621c20e637
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-02-24 13:45:33 +0600

    Update compute.py

--- compute.py
+++ compute.py
@@ -8,3 +8,8 @@ def compute_iter(nw, current_iter, theta):
         neighbor_thoughts = [nw[n]["state"] for n in value["neighbors"]]
 
         u_minus, u_plus = _generate_utility(neighbor_thoughts, eps_minus, eps_plus)
+
+        selected_state = 1 if u_plus > u_minus else -1
+        # Сохраняем выбранное состояние и итерацию, когда был сделан выбор
+        nw[key]["selected_state"] = selected_state
+        nw[key]["selected_iter"] = current_iter
