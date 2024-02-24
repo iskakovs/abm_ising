97bec9ee5af44ed9b73642d3fee7aa327d3e3335
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-02-24 12:53:09 +0600

    Update runner.py

--- runner.py
+++ runner.py
@@ -8,3 +8,9 @@ import network
 nw = network.init_network(graph_type='complete')
 
 theta = 5  # Количество периодов до того, как спин поменяет состояние (можно поменять)
+
+for i in range(500):
+    compute.compute_iter(nw, current_iter=i, theta=theta)
+    compute.finish_iter(nw, current_iter=i, theta=theta)
+
+
