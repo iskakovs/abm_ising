d3dff58051203666dfb0a48638971169897aea82
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-02-24 13:44:15 +0600

    Update compute.py

--- compute.py
+++ compute.py
@@ -2,3 +2,8 @@ import numpy as np
 
 import state
 
+def compute_iter(nw, current_iter, theta):
+    for key, value in nw.items():
+        eps_minus, eps_plus = _generate_epsilon()
+        neighbor_thoughts = [nw[n]["state"] for n in value["neighbors"]]
+
