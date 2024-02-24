1b50a0d8fc4c52dce1837a7f53c470004d31c0b5
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-02-24 13:02:43 +0600

    Update network.py

--- network.py
+++ network.py
@@ -6,3 +6,7 @@ import state
 def init_network(graph_type='barabasi_albert'):
     N = state.global_state["N"]
     M = state.global_state["M"]
+    if graph_type == 'complete':
+    G = nx.complete_graph(N)
+    else:  # По умолчанию граф Барабаши-Альберта
+    G = nx.barabasi_albert_graph(N, M, seed=42)
